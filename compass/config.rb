a60f22efb68b4d9101cff6b92f689b7c914230f8
Nelson Chen <crazysim@gmail.com>
2013-04-11 00:11:47 -0700

    add compass configuration

---
+++ compass/config.rb
@@ -0,0 +1,11 @@
+http_path    = '/'
+project_path = '.'
+css_dir      = 'content/assets/stylesheets'
+sass_dir     = 'content/assets/stylesheets'
+images_dir   = 'content/assets/images'
+relative_assets = true
+
+# when using SCSS:
+sass_options = {
+  syntax: :scss
+}
